e6037614f0521a8ebea3bdf3bd5d226c90254730
Brylan Gannon <brylan.gannon112@gmail.com>
2022-08-05 17:06:38 -0600

    Test: Landing page should have login button
    clicking it should bring them to sessions#new

--- spec/features/landing/landing_page_spec.rb
+++ spec/features/landing/landing_page_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe 'The Landing Page' do
       click_button 'Create an Account'
       expect(current_path).to eq('/users/new')
     end
+
+    it 'should have a button to login' do
+      visit '/'
+
+      click_button 'Log in'
+      expect(current_path).to eq('/sessions/new')
+    end
   end
 end
